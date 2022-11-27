420aff5b2cbe7ca8cc33afb8d91b7e95c44130d3
firstaroyand <firstaroyan09@gmail.com>
2022-11-27 15:39:00 +0700

    feat: separate icon mapper function

---
+++ src/utils/iconMapper.tsx
@@ -0,0 +1,53 @@
+import {
+  PhoneIcon,
+  UserIcon,
+  BriefcaseIcon,
+  BugAntIcon,
+  DocumentTextIcon,
+  BookOpenIcon,
+  CalculatorIcon,
+  BanknotesIcon,
+  WrenchScrewdriverIcon,
+  IdentificationIcon,
+  MapIcon,
+  Cog6ToothIcon,
+  UserGroupIcon,
+  HandThumbUpIcon,
+} from '@heroicons/react/24/solid';
+
+const iconMapper = (icon: string) => {
+  switch (icon) {
+    case 'fa-phone':
+      return <PhoneIcon width={16} />;
+    case 'fa-user':
+      return <UserIcon width={16} />;
+    case 'fa-user-secret':
+      return <BriefcaseIcon width={16} />;
+    case 'fa-bug':
+      return <BugAntIcon width={16} />;
+    case 'fa-list-alt':
+      return <DocumentTextIcon width={16} />;
+    case 'fa-address-book':
+      return <BookOpenIcon width={16} />;
+    case 'fa-calculator':
+      return <CalculatorIcon width={16} />;
+    case 'fa-money-bill-wave':
+      return <BanknotesIcon width={16} />;
+    case 'fa-tractor':
+      return <WrenchScrewdriverIcon width={16} />;
+    case 'fa-id-card':
+      return <IdentificationIcon width={16} />;
+    case 'fa-map':
+      return <MapIcon width={16} />;
+    case 'fa-cog':
+      return <Cog6ToothIcon width={16} />;
+    case 'fa-users':
+      return <UserGroupIcon width={16} />;
+    case 'fa-angellist':
+      return <HandThumbUpIcon width={16} />;
+    default:
+      break;
+  }
+};
+
+export default iconMapper;
